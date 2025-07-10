f64c02bfed7ab32cab020343ccdc63c9a3ee56e9
Aneeskhan46 <aneeskhan86305@gmail.com>
2025-07-10 12:25:07 +0530

    add url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Homepage from './landing_page/home/homepage'
+import Homepage from './landing_page/home/Homepage.jsx'
 // import './App.css'
 
 
